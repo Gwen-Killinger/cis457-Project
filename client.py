3638c1f71d581de598da5fa7d523b37b786750d6
Gwen-Killinger <jkillinger14@gmail.com>
2026-02-09 13:07:14 -0500

    Initial progress, program works with client and server connection, server doen't automatically close yet.

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 #client.py
 import socket
+
+IP_Port = ('127.0.0.1', 8008)
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mysocket.connect(IP_Port)
+print(f'== Connected to {IP_Port[0]} ==')
+
+
+message = input('Message: ')
+mysocket.send(message.encode())
+
+payload = mysocket.recv(1024)
+print(f'Server says: {payload.decode()}')
--- server.py
+++ server.py
@@ -1,2 +1,18 @@
 #server.py
 import socket
+
+IP_Port = ('127.0.0.1', 8008)
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.bind(IP_Port)
+mysocket.listen()
+
+while True:
+    connection, address = mysocket.accept()
+    client_IP = address[0]
+    print(f'A TCP connection is opened for {client_IP}')
+
+    received_data = connection.recv(1024)
+    print(f'{client_IP} said: {received_data.decode()}')
+
+    connection.send('Goodbye client!'.encode())
