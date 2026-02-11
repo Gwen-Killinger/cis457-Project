72e38c1509fb33b915fb1d882e8e8c5c3169d823
Gwen-Killinger <jkillinger14@gmail.com>
2026-02-11 10:01:16 -0500

    Fixed issue with server not closing after replying to client message.

--- server.py
+++ server.py
@@ -7,12 +7,12 @@ mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysocket.bind(IP_Port)
 mysocket.listen()
 
-while True:
-    connection, address = mysocket.accept()
-    client_IP = address[0]
-    print(f'A TCP connection is opened for {client_IP}')
+connection, address = mysocket.accept()
+client_IP = address[0]
+print(f'A TCP connection is opened for {client_IP}')
 
-    received_data = connection.recv(1024)
-    print(f'{client_IP} said: {received_data.decode()}')
+received_data = connection.recv(1024)
+print(f'{client_IP} said: {received_data.decode()}')
+connection.send('Goodbye client!'.encode())
 
-    connection.send('Goodbye client!'.encode())
+mysocket.close()
